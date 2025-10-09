dc292b1fb5209184bdba9ae363cf4bdbb1548b5c
Adrian Jacobsen Lund <adriajl@stud.ntnu.no>
2025-10-09 17:30:54 +0200

    Add initial hand_tracker.py

---
+++ hand_tracker.py
@@ -0,0 +1,14 @@
+from cvzone.HandTrackingModule import HandDetector
+
+class HandTracker:
+    def __init__(self, max_hands=1, detection_confidence=0.8):
+        self.detector = HandDetector(maxHands=max_hands, detectionCon=detection_confidence)
+
+    def find_hand(self, img):
+        hands, img = self.detector.findHands(img)
+        if hands:
+            hand = hands[0]
+            lmList = hand["lmList"]  # 21 håndlandmarks
+            fingers = self.detector.fingersUp(hand)
+            return lmList, fingers, img
+        return None, None, img
